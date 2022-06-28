21afa68f3558b61cb54840e0c4e41780c953fe4e
Sweta-Das <dassweta1997@gmail.com>
2022-06-28 13:30:47 +0545

    first commit

---
+++ Main.java
@@ -0,0 +1,37 @@
+public class main {
+
+    public static void main(String args[]) {
+        String input = args[0];
+        Playable playable = null;
+        switch (input) {
+            case "football":
+                playable = new Football();
+                break;
+    
+            case "basketball":
+                playable = new Basketball();
+                break;
+    
+            case "guitar":
+                playable = new Guitar();
+                break;
+        
+            case "violin":
+                playable = new Violin();
+                break;
+            
+        }
+        play(playable);
+    }   
+    
+    public static void play (Playable playable)
+        playable.play();
+        playable.kick();
+        if(playable instanceof Football)
+        {
+            Football football = (Football) playable; // Typecasting 
+            football.kick(); 
+        }
+    }
+
+}
